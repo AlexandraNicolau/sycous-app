30e11d097051d4e90dd3d802c64bae8f64febdb7
AlexandraNicolau <38588319+AlexandraNicolau@users.noreply.github.com>
2021-05-09 20:09:10 +0100

    Displaying consumer data on Front End (#3)
    * Retriving consumers for each location in an array
    
    * render list of consumers within the main section
    
    * Cleaning up styles and creating list headings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const getConsumers = (data) => {
+  const consumers = [];
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+  data.map(item => consumers.push(...item.consumers));
+  return consumers;
+}
+
 function App() {
   const [data, setData] = useState([]);
 
@@ -9,15 +16,42 @@ function App() {
     const result = await axios(
       'http://localhost:3000/locations',
     );
-    console.log(result.data)
-    setData(result.data);
+
+    if (result.status === 200) {
+      const consumers = getConsumers(result.data)
+      // console.log(consumers)
+      setData(consumers);
+    }
+    else {
+      console.log("Please start the mock server")
+    }
+
   }, []);
+
+  const consumersList = data.map((consumer) => {
+    return <li className="consumer" key={consumer.consumerId}>
+     <div> {consumer.name}</div>
+      <div>{consumer.email}</div>
+      <div>{consumer.phoneNumber}</div>
+      <div>{consumer.occupationDate}</div>
+    </li>
+  });
 
   return (
     <div className="app">
       <header>Header</header>
-      <main>Main</main>
-      <footer>Footer</footer>    
+      <main>
+        <ul>
+          <li className="consumer">
+            <div>Name</div>
+            <div>Email</div>
+            <div>Phone Number</div>
+            <div>Occupation Date</div>
+          </li>
+          {consumersList}
+        </ul>
+      </main>
+      <footer>Footer</footer>
     </div>
   );
 }
