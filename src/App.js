b8f3d7bb194f297122e3f7070a4905bc4547fae6
AlexandraNicolau <38588319+AlexandraNicolau@users.noreply.github.com>
2021-05-09 19:04:25 +0100

    Fetching data (#2)
    * created mock API server
    
    * Added moc.API node modules to git ignore
    
    * using Axios to fetch data from mock server
    
    * Removing node modules folder accidentally added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(async () => {
+    const result = await axios(
+      'http://localhost:3000/locations',
+    );
+    console.log(result.data)
+    setData(result.data);
+  }, []);
+
   return (
     <div className="app">
       <header>Header</header>
