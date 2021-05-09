a242a3c36ec3002bebbeace8c75225aad0cc3a2e
Alexandra Nicolau <alexandra.tnicolau@gmail.com>
2021-05-09 20:01:03 +0100

    render list of consumers within the main section

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const getConsumers = (data) => {
-  const consumers = data.map(item => item.consumers);
-  console.log(consumers)
+  const consumers = [];
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+  data.map(item => consumers.push(...item.consumers));
+  return consumers;
 }
 
 function App() {
@@ -14,22 +16,36 @@ function App() {
     const result = await axios(
       'http://localhost:3000/locations',
     );
+
     if (result.status === 200) {
-      getConsumers(result.data)
+      const consumers = getConsumers(result.data)
+      // console.log(consumers)
+      setData(consumers);
     }
     else {
       console.log("Please start the mock server")
     }
 
-    // console.log(result.data)
-    // setData(result.data);
   }, []);
+
+  const consumersList = data.map((consumer) => {
+    return <li className="consumer"key={consumer.consumerId}>
+     <div> {consumer.name}</div>
+      <div>{consumer.email}</div>
+      <div>{consumer.phoneNumber}</div>
+      <div>{consumer.occupationDate}</div>
+    </li>
+  });
 
   return (
     <div className="app">
       <header>Header</header>
-      <main>Main</main>
-      <footer>Footer</footer>    
+      <main>
+        <ul>
+          {consumersList}
+        </ul>
+      </main>
+      <footer>Footer</footer>
     </div>
   );
 }
